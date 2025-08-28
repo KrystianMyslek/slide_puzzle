ec2152ff793e78ee8693f32442d203a25bf5153e
krystian <krystian@debian.krystian.net>
2025-08-29 00:18:24 +0200

    Set class

--- src/components/Set.jsx
+++ src/components/Set.jsx
@@ -1,42 +1,116 @@
-
+import { useEffect } from 'react'
 import Cons from './../Cons';
 
 export default function Set(props) {
 
     var bsh = props.boardSizeHandle;
 
+    class BoardSize {
+        boardXSize;
+        boardYSize;
+        
+        constructor(boardXSize, boardYSize) {
+            this.boardXSize = boardXSize
+            this.boardYSize = boardYSize
+            this.init()
+        }
+
+        init() {
+            this.disableXButton()
+            this.disableYButton()
+        }
+
+        setBoardXSize() {
+            props.boardSizeHandle.setBoardXSize(this.boardXSize);
+            props.boardSizeHandle.setEmptySpace(
+                [this.boardXSize-1, this.boardYSize-1]
+            );
+        }
+        
+        setBoardYSize() {
+            props.boardSizeHandle.setBoardYSize(this.boardYSize);
+            props.boardSizeHandle.setEmptySpace(
+                [this.boardXSize-1, this.boardYSize-1]
+            );
+        }
+
+        disableXButton() {
+            if (this.boardXSize <= 2) {
+                document.getElementById("set_board_x_size_button_minus").disabled = true;
+            } else {
+                document.getElementById("set_board_x_size_button_minus").disabled = false;
+            }
+        }
+
+        disableYButton() {
+            if (this.boardYSize <= 2) {
+                document.getElementById("set_board_y_size_button_minus").disabled = true;
+            } else {
+                document.getElementById("set_board_y_size_button_minus").disabled = false;
+            }
+        }
+
+        incrementXSize() {
+            this.boardXSize++
+            this.setBoardXSize()
+            this.disableXButton()
+        }
+
+        decrementXSize() {
+            this.boardXSize--
+            this.setBoardXSize()
+            this.disableXButton()
+        }
+
+        incrementYSize() {
+            this.boardYSize++
+            this.setBoardYSize()
+            this.disableYButton()
+        }
+
+        decrementYSize() {
+            this.boardYSize--
+            this.setBoardYSize()
+            this.disableYButton()
+        }
+    }
+
+    useEffect(() => {
+        window.bs = new BoardSize(bsh.boardXSize, bsh.boardYSize);
+    }, []); 
+
     return (
         <>
             <div className='config'>
                 <div>
                     <span>ILOŚĆ WIERSZY</span>
-                    <button onClick={() => {
-                        bsh.setBoardXSize(bsh.boardXSize+1)
+                    <button id="set_board_x_size_button_minus" onClick={() => {
+                        bs.decrementXSize()
                     }}>
-                        +
+                        -
                     </button>
                     <span>{bsh.boardXSize}</span>
-                    <button onClick={() => {
-                        bsh.setBoardXSize(bsh.boardXSize-1)
-                    }}>
-                        -
-                    </button>
-                    <span>ILOŚĆ KOLUMN</span>
-                    <button onClick={() => {
-                        bsh.setBoardYSize(bsh.boardYSize+1)
+                    <button id="set_board_x_size_button_plus" onClick={() => {
+                        bs.incrementXSize()
                     }}>
                         +
                     </button>
-                    <span>{bsh.boardYSize}</span>
-                    <button onClick={() => {
-                        bsh.setBoardYSize(bsh.boardYSize-1)
+                    <span>ILOŚĆ KOLUMN</span>
+                    <button id="set_board_y_size_button_minus" onClick={() => {
+                        bs.decrementYSize()
                     }}>
                         -
+                    </button>
+                    <span>{bsh.boardYSize}</span>
+                    <button id="set_board_y_size_button_plus" onClick={() => {
+                        bs.incrementYSize()
+                    }}>
+                        +
                     </button>
                 </div>
                 <div>
                     <button className="start_game" onClick={() => {
-                        bsh.setGameStatus(Cons.GAME_STATUS_PLAY)
+                        bsh.setGameStatus(Cons.GAME_STATUS_ROLL)
                     }}>
                         START GAME
                     </button>
