c583924f0e6685a8e5488db07278e1a578c0a4c5
krystian <krystian@debian.krystian.net>
2025-09-03 08:48:00 +0200

    generate random board script

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
-import { useState, useEffect, Fragment } from 'react'
+import { useState, Fragment } from 'react'
 import './css/App.css'
 
 import Cons from './Cons';
 import Board from './components/Board';
 import Set from './components/Set';
+import Win from './components/Win';
 
 function App() {
+
+  var defaultBoardXSize = 4;
+  var defaultBoardYSize = 2;
+
   const [gameStatus, setGameStatus] = useState(Cons.GAME_STATUS_SET);
 
-  const [boardXSize, setBoardXSize] = useState(4);
-  const [boardYSize, setBoardYSize] = useState(3);
+  const [boardXSize, setBoardXSize] = useState(defaultBoardXSize);
+  const [boardYSize, setBoardYSize] = useState(defaultBoardYSize);
   
-  const [emptySpace, setEmptySpace] = useState([0,0]);
+  const [emptySpace, setEmptySpace] = useState([boardXSize-1,boardYSize-1]);
   const [puzzlePlaces, setPuzzlePlaces] = useState();
 
   function generateSet() {
     var boardSizeHandle = {
+      emptySpace,
       boardXSize,
       boardYSize,
+      setEmptySpace,
       setBoardXSize,
       setBoardYSize,
       setGameStatus
@@ -41,22 +48,101 @@ function App() {
     return <Board size={size} boardHandle={boardHandle} />
   }
 
-  function rollPuzzlePlaces () {
-    var newPuzzlePlaces = new Array(boardXSize * boardYSize);
+  function generateWin() {
+    return <Win />
+  }
 
-    var pos = 0
-    for (var i=0; i<boardXSize; i++) {
-      for (var j=0; j<boardYSize; j++) {
-        newPuzzlePlaces[pos] = {
-          x: i,
-          y: j,
-          value: pos
+  function checkIfGameWon(_puzzlePlaces) {
+    if (_puzzlePlaces == undefined) {
+      return false
+    }
+
+    var game_won = true;
+    _puzzlePlaces.slice(0,-1).forEach((puzzle, index) => {
+      if ((index+1) != puzzle.value) 
+        game_won = false
+    });
+
+    if (gameStatus == Cons.GAME_STATUS_PLAY && game_won) {
+      setGameStatus(Cons.GAME_STATUS_WIN)
+    }
+
+    return game_won;
+  }
+
+  function rollPuzzlePlaces () {
+    var usedPlaces = [];
+    
+    var size = boardXSize * boardYSize;
+    var newPuzzlePlaces = new Array(size);
+    
+    function generateRandomPlace(min, max) {
+      var num = Math.floor(Math.random() * (max - min + 1)) + min;
+      return (usedPlaces.includes(num)) ? generateRandomPlace(min, max) : num;
+    }
+    
+    function generateRandomBoard() {
+      usedPlaces = []
+
+      newPuzzlePlaces[(size-1)] = {
+        x: boardXSize,
+        y: boardYSize,
+        value: 0
+      }
+
+      usedPlaces.push(0)
+
+      var pos = 0
+      var value
+      for (var i=0; i<boardXSize; i++) {
+        for (var j=0; j<boardYSize; j++) {
+          if (i==0 && j==0) continue
+
+          value = generateRandomPlace(0, (size-1))
+          usedPlaces.push(value)
+
+          if (value == 0) {
+            setEmptySpace([i,j])
+          }
+
+          newPuzzlePlaces[pos] = {
+            x: i,
+            y: j,
+            value: value
+          }
+          
+          pos++
         }
-        
-        pos++;
       }
     }
 
+    function countInversions() {
+      var inversions = 0;
+
+      newPuzzlePlaces.forEach((puzzle, index) => {
+        if (puzzle.value != 0) {
+          newPuzzlePlaces.slice(index+1).forEach(checkPuzzle => {
+            if (checkPuzzle.value != 0 && puzzle.value > checkPuzzle.value) {
+              inversions++
+            }
+          })
+        }
+      })
+
+      return inversions;
+    }
+
+    var roll = true
+    while (roll) {
+      generateRandomBoard();
+      if (countInversions() % 2 == 0) {
+        if (checkIfGameWon(newPuzzlePlaces) == false) {
+          roll = false
+        }
+      }
+    }
+    
+    setGameStatus(Cons.GAME_STATUS_PLAY)
     setPuzzlePlaces(newPuzzlePlaces);
   }
 
@@ -66,20 +152,23 @@ function App() {
       app = generateSet();
       
       break;
+    case Cons.GAME_STATUS_ROLL:
+      rollPuzzlePlaces();
       
+      break;
     case Cons.GAME_STATUS_PLAY:
+      checkIfGameWon(puzzlePlaces);
+      
       app = generateBoard(boardXSize, boardYSize);
       
+      break;
+    case Cons.GAME_STATUS_WIN:
+      app = generateWin();
+
       break;
     default:
       break;
   }
-    
-  useEffect(() => {
-    if (gameStatus == Cons.GAME_STATUS_PLAY) {
-      rollPuzzlePlaces();
-    }
-  }, [gameStatus]);
 
   return (
     <>
--- src/Cons.jsx
+++ src/Cons.jsx
@@ -1,6 +1,7 @@
 
 export default {
     GAME_STATUS_SET: 1,
-    GAME_STATUS_PLAY: 2,
-    GAME_STATUS_WIN: 3
+    GAME_STATUS_ROLL: 2,
+    GAME_STATUS_PLAY: 3,
+    GAME_STATUS_WIN: 4
 }
